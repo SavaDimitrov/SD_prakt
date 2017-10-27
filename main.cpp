9d8b790da28d59415363eb8e085cac29ed0a20ba
SavaDimitrov <savad.9310@gmail.com>
2017-10-27 14:58:19 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <ctime>
 
 using namespace std;
+
+template <typename T>
+class dynArray {
+private:
+	T* arr;
+	unsigned maxNumOfCities;
+	int currentCity;
+	
+	void resizeArray() {
+		maxNumOfCities *= 2;
+		
+		T* tmpArray = new T[maxNumOfCities];
+		for (int i = 0; i < currentCity; i++)
+		{
+			tmpArray[i] = arr[i];
+		}
+
+		deleteArr();
+		//arr = new T[maxNumOfCities];
+		arr = tmpArray;
+	}
+
+	void deleteArr() {
+		delete arr;
+		arr = nullptr;
+	}
+public:
+	dynArray(unsigned _maxNumOfCities, unsigned _currentCity) {
+		arr = new T[_maxNumOfCities];
+		_currentCity = 0;
+	}
+
+	void addCity(const T& newCity) {
+		if (currentCity == maxNumOfCities) {
+			resizeArray();
+		}
+
+		arr[currentCity] = newCity;
+		currentCity++;
+	}
+	/*dynArray& operator=(const T& other) {
+		maxNumOfCities = other.getNumOfCities();
+		currentCity = other.getCurrCity();
+		//TODO
+	}*/
+
+	/*unsigned getNumOfCities() { return maxNumOfCities; }
+	int getCurrCity() { return currentCity; }*/
+
+};
+
 
 class City {
 	char* name;
@@ -21,6 +73,15 @@ public:
 
 	}
 
+	City& operator=(const City& other) {
+		if (this != &other) {
+			removeCity();
+		}
+
+		this->setName(other.getName());
+		this->population = other.getPopulation();
+	}
+
 	void setName(char* newName) {
 		if (name != nullptr) {
 			removeCity();
@@ -31,7 +92,7 @@ public:
 		strcpy(name, newName);
 	}
 
-	const char* getName() const { return name; }
+	char* getName() const { return name; }
 	int getPopulation() const { return population; }
 
 	~City() {
@@ -39,57 +100,66 @@ public:
 	}
 };
 
-void writeToBin(City* city, int numOfCities, char* fileName) {
-	ofstream myFile(fileName, ios::out | ios::binary);
 
-	if (fileName) {
-		for (int i = 0; i < numOfCities; i++)
-		{
-			int tmp = city[i].getPopulation();
 
-			int cityNameLen = strlen(city[i].getName()) + 1;
-			myFile.write((char*)&cityNameLen, sizeof(cityNameLen));
-			myFile.write((char*)city[i].getName(), cityNameLen);
-			myFile.write((char*)&tmp, sizeof(city[i].getPopulation()));
-		}
-	}
-	else cout << "Error!\n";
-
-	myFile.close();
-}
-
-void readFromBin(char* fileName) {
-	ifstream myFile(fileName, ios::in | ios::binary);
+void readFromBin(char* fileName, dynArray<City> cities) {
+	ifstream myFile(fileName, ios::in);
 
 	if (myFile) {
 		while (!myFile.eof()) {
-			int length;
-			myFile.read((char*)&length, sizeof(length));
 
-			char* city = new char[length + 1];
-			myFile.read(city, length);
+			string city;
+			myFile >> city;
 			int population;
 			myFile.read((char*)&population, sizeof(population));
 
-			cout << "City name:" << city << '\n' << "It's population: " << population << endl;
+			City newCity(city, population);
 
-			delete city;
+			cities.addCity(newCity);
+
+			//delete city;
 		}
 	}
 
 	myFile.close();
 }
 
-int main() {
 
-	ifstream binFile("cities.dat", ios::in | ios::binary);
 
-	City city1("Sofia", 5000);
-	City city2("Plovdiv", 2000);
-	int len = 2;
-	City cities[] = { city1, city2 };
 
-	writeToBin(cities, len, "cities.dat");
+int main(int argc, char* argv[]) {
+
+	//clock_t start;
+	//double duration;
+	//start = clock();
+
+	//int n = 100000;
+
+	//for (int index = 0; index < n; index++)
+	//{
+	//	for (int index1 = 0; index1 < n; index1++)
+	//	{
+	//		//26,... sec (n^2 - slojnost na algorituma)
+	//	}
+	//}
+
+	//for (int index = 0; index < n; index++)
+	//{
+	//	for (int index1 = 0; index1 < n; index1 *= 2)
+	//	{
+	//		//0,003 sec (n*log(n) - slojnost na algorituma)
+	//	}
+	//}
+
+	////sbora na slojnosta e ~26
+
+	//for (int index = 0; index < n; index *= 2)
+	//{
+	//	for (int index1 = 0; index1 < n; index1 *= 2)
+	//	{
+	//		//0,000000... - slojnost na algoritma
+	//	}
+	//}
 
 	return 0;
 }
